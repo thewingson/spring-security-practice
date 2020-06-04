080b289a7de6bfe32f4be506808bce30805b17e0
Almat <almatrakh97@gmail.com>
2020-06-04 13:48:00 +0600

    SecurityConfig

---
+++ src/main/java/kz/almat/springsecuritypractice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package kz.almat.springsecuritypractice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Almat on 04.06.2020
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
