7a11a7db9168e85ff98c6521199f4079bd88c1b5
Almat <almatrakh97@gmail.com>
2020-06-04 13:56:14 +0600

    UserDetailsService

--- src/main/java/kz/almat/springsecuritypractice/config/SecurityConfig.java
+++ src/main/java/kz/almat/springsecuritypractice/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package kz.almat.springsecuritypractice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Almat on 04.06.2020
@@ -21,5 +26,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userAlmat = User.builder()
+                .username("almat")
+                .password("almat")
+                .roles("STUDENT") //ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(userAlmat);
     }
 }
